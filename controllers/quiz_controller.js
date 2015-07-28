e47ef31487022dd23f9734ba8cabb0685b0e0194
Adrian Klinger <klinger_adrian@yahoo.com.ar>
2015-07-28 17:33:10 +0400

    Search

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,23 @@
 var models = require('../models/models.js');
 
+// Search
+exports.index = function(req, res){
+
+  if(req.query.search) {
+    var filtro  = (req.query.search || '').replace(" ", "%");
+    models.Quiz.findAll({where:["pregunta like ?", '%'+filtro+'%'],order:'pregunta ASC'}).then(function(quizes){
+      res.render('quizes/index', {quizes: quizes});
+    }).catch(function(error) { next(error);});
+
+  } else {
+
+   models.Quiz.findAll().then(function(quizes){
+       res.render('quizes/index', {quizes: quizes});
+       }).catch(function(error) { next(error);});
+  }
+};   
+
+
 // Autoload - factoriza el codigo si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
   models.Quiz.find(quizId).then(
