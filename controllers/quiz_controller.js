60826fbbdb109c5eddd7f850b5d17848834b18cf
Adrian Klinger <klinger_adrian@yahoo.com.ar>
2015-07-29 14:47:11 +0400

    Search II

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,23 +1,5 @@
 var models = require('../models/models.js');
 
-// Search
-exports.index = function(req, res){
-
-  if(req.query.search) {
-    var filtro  = (req.query.search || '').replace(" ", "%");
-    models.Quiz.findAll({where:["pregunta like ?", '%'+filtro+'%'],order:'pregunta ASC'}).then(function(quizes){
-      res.render('quizes/index', {quizes: quizes});
-    }).catch(function(error) { next(error);});
-
-  } else {
-
-   models.Quiz.findAll().then(function(quizes){
-       res.render('quizes/index', {quizes: quizes});
-       }).catch(function(error) { next(error);});
-  }
-};   
-
-
 // Autoload - factoriza el codigo si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
   models.Quiz.find(quizId).then(
@@ -31,11 +13,24 @@ exports.load = function(req, res, next, quizId) {
 };
 
 // GET /quizes
-exports.index = function(req, res) {  
-  models.Quiz.findAll().then(function(quizes) {
-      res.render('quizes/index.ejs', {quizes: quizes});
-    }
-  ).catch(function(error){next(error);});
+exports.index = function(req, res) {
+  if(req.query.search){    
+    var search  = '%'+ req.query.search.replace(/ /g, '%') + '%';
+
+    models.Quiz.findAll({where: ["pregunta like ?", search],order: 'pregunta ASC'}).then(
+      function(quizes){
+        res.render('quizes/index.ejs', {quizes: quizes});
+      }
+    ).catch(function(error){next(error);});
+
+  } else {
+    models.Quiz.findAll().then(
+      function(quizes){
+        res.render('quizes/index.ejs', {quizes: quizes});
+      }
+    ).catch(function(error){next(error);});
+
+  }
 };
 
 // GET /quizes/:id
